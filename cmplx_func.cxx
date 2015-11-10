da74f143d0f9581ff605b71f6bbf1f5e69f75cab
Philipp Schulte <pschulte@pc08.pool.tp1.uni-duesseldorf.de>
2015-11-10 16:37:38 +0100

    Lab3 closed.

--- cmplx_func.cxx
+++ cmplx_func.cxx
@@ -1,7 +1,69 @@
 #include <iostream>
 #include <complex>
+#include <cmath>
+
+using namespace std
+
+;
+/*
+Function to iterate the compley number array of Phi
+
+*/
+complex<double>calcz(double cphi, complex<double> cunit) {
+  complex<double> cz;
+
+  cz = tan(cphi + (cphi * cunit));
+
+  return cz;
+
+}
+
+double realz(complex<double> cz) {
+
+   return real(cz);
+}
+
+double imagz(complex<double> cz) {
+
+   return imag(cz);
+
+}
+
+double absolutez(complex<double> cz) {
+
+   return abs(cz);
+}
+
 
 int main(){
+
+// Declaration of used vars:
+complex<double> ii = complex<double>(0.0, 1.0); // complex unit
+int N= 20;  //Number of itertions
+
+  /* cout << "Which N?: ";
+  cin >> N;
+*/
+
+complex<double> z[N];
+double phi[N];
+double rez[N];
+double imz[N];
+double absz[N];
+double phi0 = (-1 * M_PI);
+double dphi = (2* M_PI) / (N-1);  // Delta Phi
+
+    
+  for (int i=0; i<= N-1; i++) {
+    phi[i] = phi0 + (i * dphi);
+    z[i] = calcz(phi[i], ii);
+    rez[i] = realz(z[i]);
+    imz[i] = imagz(z[i]);
+    absz[i] = absolutez(z[i]);
+    cout << phi[i] << "		" << z[i] << "	" << rez[i] << "	" << imz[i] << "	" << absz[i] <<  endl;
+
+  }
+
 
   // Example how to declare a single complex variable:
   complex<double> c;
--- newton.cxx
+++ newton.cxx
@@ -1,13 +1,38 @@
 #include <iostream>
 #include <complex>
+#include <cmath>
+
+using namespace std;
 
 int main(){
+complex<double> z0;
+double rez;
+double imz;
+double skip = 1.0 * exp(-8);
 
-  // Example how to declare a single complex variable:
-  complex<double> c;
+  for (double re= -2; re <= 2; re +=0.01) {
+    for (double im = -2; im <= 2; im +=0.01) {
+      z0 = complex<double>(re, im);
+      complex<double> zn = z0 - (((z0*z0*z0)-1.0)/(3.0 * (z0*z0)));
+     
+      rez = real(z0);
+      imz = imag(z0);
 
-  // d = 1 + 2i 
-  complex<double> d = complex<double>(1.0, 2.0); 
+	
+      double diffz = abs(zn-z0);
+      int i = 1;
+      while ((diffz > skip) and (i <= 399)) {
+        i++;
+        z0 = zn;
+        zn = z0 - (((z0*z0*z0)-1.0)/(3.0 * (z0*z0)));
+        diffz = abs(zn-z0);
+      }
+      
+      
+      cout << rez << "\t" << imz << "\t" << i <<endl;
+
+    }
+  }
 
   return 0;
 }
